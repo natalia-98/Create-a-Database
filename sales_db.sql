9d8e47135a86ab7eecfb2167f6b856d2e46c1ecb
american hot dog <81496983+natalia-98@users.noreply.github.com>
2022-02-08 21:22:47 -0500

    add files sql

---
+++ sales_db.sql
@@ -0,0 +1,74 @@
+-- Create sales database 
+CREATE DATABASE sales;
+USE sales;
+
+-- Create table 1: orders
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders (
+	order_number_line VARCHAR(45),
+    quantity INT,
+    price FLOAT,
+    sale_total FLOAT, 
+    order_date DATETIME,
+    quarter INT,
+    product_code VARCHAR(45),
+    customer_name VARCHAR(45),
+    deal_size VARCHAR(45),
+    PRIMARY KEY (order_number_line)
+    );
+
+-- Create table 2: customers
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers (
+	customer_name VARCHAR(45),
+    phone VARCHAR(45),
+    address VARCHAR(100),
+    city VARCHAR(45),
+    state VARCHAR(45),
+    country VARCHAR(45),
+    contact_name VARCHAR(45),
+    PRIMARY KEY (customer_name)
+    );
+
+-- Create table 3: products
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+	product_code VARCHAR(45),
+    msrp FLOAT,
+    product_line VARCHAR(45),
+    PRIMARY KEY (product_code)
+    );
+
+-- Create table 4; order_status
+DROP TABLE IF EXISTS order_status;
+CREATE TABLE order_status (
+	order_number_line VARCHAR(45),
+    status VARCHAR(45),
+    FOREIGN KEY (order_number_line) REFERENCES orders(order_number_line)
+    );
+  
+-- NOTE: Import data into the empty data tables BEFORE adding foreign key constraints or else an error will occur.
+-- Alter table 1: orders to add foreign keys
+ALTER TABLE orders
+ADD FOREIGN KEY (customer_name) REFERENCES customers(customer_name);
+
+ALTER TABLE orders
+ADD FOREIGN KEY (product_code) REFERENCES products(product_code);
+
+-- Import data into table: orders
+-- SHOW GLOBAL VARIABLES LIKE 'local_infile';
+-- SET GLOBAL local_infile = 'ON';
+-- set global local_infile=true;
+-- SHOW GLOBAL VARIABLES LIKE 'local_infile';
+
+-- LOAD DATA INFILE 'orders.csv'
+-- INTO TABLE orders
+-- FIELDS TERMINATED BY ','
+-- ENCLOSED BY  '"'
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 ROWS;
+
+
+
+	
+
